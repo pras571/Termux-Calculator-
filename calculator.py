47181cabd7fb3a85a09a4308e37f083520efd711
pras571 <ggithub72@gmail.com>
2025-05-14 18:50:38 +0545

    Termux Calculator
    This calculator add subtract and divide in one time

---
+++ calculator.py
@@ -0,0 +1,22 @@
+num1=int(input("ENTER YOUR FIRST NUMBER : "))
+
+num2=int(input("ENTER YOUR SECOND NUMBER : "))
+
+print(f"add result is:{num1+num2}")
+
+
+
+print(f"subtract result is:{num1-num2}")
+
+
+
+print(f"divide result is:{num1*num2}")
+
+
+
+res=num1+num2
+
+res=num1-num2
+
+res=num1*num2
+
